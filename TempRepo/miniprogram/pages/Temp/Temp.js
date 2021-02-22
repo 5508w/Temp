0b5e73335d1fec28eafc334cec2b95791d64c33a
5508w <51290457+5508w@users.noreply.github.com>
2021-02-22 22:14:56 +0800

    Add files via upload

--- TempRepo/miniprogram/pages/Temp/Temp.js
+++ TempRepo/miniprogram/pages/Temp/Temp.js
@@ -1,5 +1,7 @@
 var util = require('../../utils/utils.js'); 
 var amapFile = require('../../libs/amap-wx.130');
+const wxApi = require('../../utils/auth.js')
+const app = getApp()
 var markersData = {
  latitude: '',//纬度
  longitude: '',//经度
@@ -9,13 +11,58 @@ var markersData = {
  Page({
   //初始化数据
   data: {
+    id:"",
     temp: "",
-    region:"正在读取中",
+    region:"",
     Date:"",
     time:"",
+    openid:""
   },
-  onLoad: function () {  
+  onLoad: function () { 
+    var that = this
+    wx.login({
+      success (res) {
+        if (res.code) {
+          //发起网络请求
+            data: {
+              code: res.code
+            }
+            // console.log("!!!"+res.code);
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+      
+    }),
+    // wx.login({
+    //   success: function(data) {
+    //     console.log('获取登录 Code：' + data.code)
+    //     var postData = {
+    //       code: data.code
+    //     };
+    //     wx.request({
+    //       url: 'http://www.localhost:8080/TempServlet',//注意改成自己的服务器请求地址哦！
+    //       data: postData,
+    //       method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    //       header: {
+    //         'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+    //       },
+    //       success: function(res) {
+    //         //回调处理
+    //         console.log('getOpenID-OK!');
+    //         console.log(res.data);
+    //       },
+    //       fail: function(error) {
+    //         console.log(error);
+    //       }
+    //     })
+    //   },
+    //   fail: function() {
+    //     console('登录获取Code失败！');
+    //   }
+    // }),
     this.loadInfo();
+    
     var date = formatDate(new Date());
     var time = formatTime(new Date());
     this.setData({  
@@ -26,56 +73,91 @@ var markersData = {
   //定位提交按钮
   loadInfo: function(){
     var that=this;
-    wx.getLocation({
-     type: 'gcj02', //返回可以用于wx.openLocation的经纬度
-     success: function (res) {
-      var latitude = res.latitude//维度
-      var longitude = res.longitude//经度
-      console.log(res);
-      that.loadCity(latitude,longitude);
-     }
+    var latitude
+    var longitude
+    wxApi.getWxSetting('userLocation').then(()=>{
+      wx.getLocation({
+        type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+        success: function (res) {
+          latitude = res.latitude,//维度
+          longitude = res.longitude,//经度
+            // console.log(res);
+          
+          that.loadCity(latitude,longitude);
+        },
+        fail: err => {
+          wx.showToast({
+            title: '检查手机定位权限',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+      })
     })
    },
    //把当前位置的经纬度传给高德地图，调用高德API获取当前地理位置，天气情况等信息
    loadCity: function (latitude, longitude){
     var that=this;
     var myAmapFun = new amapFile.AMapWX({ key: markersData.key });
+    
     myAmapFun.getRegeo({
      location: '' + longitude + ',' + latitude + '',//location的格式为'经度,纬度'
      success: function (data) {
-      console.log(data);
+      // console.log(data);
       that.setData({
         region: data[0].name,
       });
      },
-     fail: function (info) { }
+     fail: function (info) {
+      wx.showToast({
+        title: info[0]+"！",
+        icon: 'none',
+        duration: 2000
+      })
+      that.setData({
+        region: info,
+      });
+      }
     });
    },
   //表单提交按钮
   formSubmit: function(e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
-    const db = wx.cloud.database()
-    db.collection('form').add({ //插入报名信息
-      data: {
-        temp: this.data.temp,
-        region:this.data.region,
-        Date:this.data.Date,
-        time:this.data.time,
-      },
-      success: function(res) {
-        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
-        console.log("插入成功" + res)
-        wx.showToast({
-          title: '成功',
-          icon: 'success',
-          duration: 2000
-        })
-        wx.navigateTo({
-          url: '/pages/lookform/lookform?form_name=' + that.data.name,
-        })
-      },
-      fail: console.error
-    })
+    var that=this;
+    console.log('form发生了submit事件，携带数据为：',this.data.temp)
+    if(this.data.temp!=""){
+      const db = wx.cloud.database()
+      db.collection('Temp').add({ //插入报名信息
+        data: {
+          id: this.data.id,
+          temp: this.data.temp,
+          region:this.data.region,
+          Date:this.data.Date,
+          time:this.data.time,
+          code:this.data.code,
+        },
+        success: function(res) {
+          // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+          console.log("插入成功" + res)
+          wx.showToast({
+            title: '成功',
+            icon: 'success',
+            duration: 2000
+          })
+          wx.navigateTo({
+            url: '/pages/lookform/lookform?form_id=' + that.data.id+'&&form_Date=' + that.data.Date
+            +'&&form_time=' + that.data.time,
+          })
+        },
+        fail: console.error
+      })
+    }else{
+      wx.showToast({
+        title: '操作失败！体温为空', // 标题
+        icon: 'none',  // 图标类型，默认success
+        duration: 1500  // 提示窗停留时间，默认1500ms
+      })
+    }
+    
   },
   //表单重置按钮
   formReset: function(e) {
@@ -105,6 +187,37 @@ var markersData = {
     var timestamp = Date.parse(new Date());
     this.setData({
       time: e.detail.value
+    })
+  },
+  idinput: function(e) {
+    console.log(e.detail.value)
+    this.setData({
+      id: e.detail.value //待插入的id字段
+    })
+  },
+  tempinput: function(e) {
+    console.log(e.detail.value)
+    this.setData({
+      temp: e.detail.value //待插入的体温字段
+    })
+  },
+  onGetopenid: function(e) {
+    this.loadInfo();
+    
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        this.setData({
+          openid: res.result.openid
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
     })
   },
 })
